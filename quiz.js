7b04a1513b981cef8ab7997fd9cfd4e963b3253a
branislav-bogosavac <branislav-bogosavac@byu.local>
2024-07-16 17:31:36 -0600

    quiz

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('main ');
-const continueBtn = document.querySelector('.continue-btn');
+const quizButtons = document.querySelectorAll('.continue-btn');
 const quizSection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
 
@@ -19,31 +19,46 @@ exitBtn.onclick = () => {
 
 }
 
-continueBtn.onclick = () => {
-    quizSection.classList.add('active');
-    popupInfo.classList.remove('active');
-    main.classList.remove('active');
-    quizBox.classList.add('active');
-    showQuestions(0);
-}
 
+quizButtons.forEach(btn => {
+    btn.onclick = () => {
+        quizSection.classList.add('active');
+        popupInfo.classList.remove('active');
+        main.classList.remove('active');
+        quizBox.classList.add('active');
+        quizChoice = btn.textContent;
+        showQuestions(0);
+    };
+});
 
+let quizChoice;
 
 let questionCount = 0;
+let questions = [];
+let userScore = 0;
 
 const nextBtn = document.querySelector('.next-btn');
 const optionList = document.querySelector('.option-list');
 
 nextBtn.onclick = () => {
-    questionCount++;
-    if (questionCount < questions.length) {
+    if (questionCount < questions.length - 1) {
+        questionCount++;
         showQuestions(questionCount);
+        nextBtn.classList.remove('active');
+
     } else {
-        console.log("End of questions");
+        console.log("Question Completed");
     }
 }
 
-function showQuestions(index) {
+async function showQuestions(index) {
+
+    questions = await getData(quizChoice);
+
+    questionCounter(questionCount + 1, questions);
+    headerScore();
+
+
     const questionText = document.querySelector('.question-text');
     questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
 
@@ -53,108 +68,63 @@ function showQuestions(index) {
     }
 
     optionList.innerHTML = optionTag;
+
+    const option = document.querySelectorAll('.option');
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute('onclick','optionSelected(this)');
+    }
+}
+
+function optionSelected(answer){
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    let allOptions = optionList.children.length;
+
+    if (userAnswer == correctAnswer){
+        answer.classList.add('correct');
+        userScore += 1;
+        headerScore();
+
+    } else {
+        answer.classList.add('incorrect');
+        for (let i = 0; i < allOptions; i++){
+            if (optionList.children[i].textContent == correctAnswer){
+                optionList.children[i].setAttribute('class','option correct');
+                // optionList.children[i].classList.add(correct);
+            }
+        }
+
+    }
+
+    for (let i = 0; i < allOptions; i++){
+        optionList.children[i].classList.add('disabled');
+    }
+
+    nextBtn.classList.add('active');
+
 }
 
 
+async function getData(quiz){
+    const res = await fetch(`${quiz}.json`);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let questions = [
-    {
-        numb: 1,
-        question: 'What is the Cyrilic equivalent for letter "A"?',
-        answer: "Аа",
-        options: [
-            "Ее",
-            "Оо",
-            "Аа",
-            "Лл"
-        ]
-    },
-    {
-        numb: 2,
-        question: 'What is the Cyrilic equivalent for sound "R"?',
-        answer: "",
-        options: [
-            "Сс",
-            "Тт",
-            "Ђђ",
-            "Рр"
-        ]
-    },
-    {
-        numb: 3,
-        question: 'What is the Cyrilic equivalent for sound "Sh" as in "smaSHed"?',
-        answer: "Шш",
-        options: [
-            "Њњ",
-            "Кк",
-            "Шш",
-            "Чч"
-        ]
-    },
-    {
-        numb: 4,
-        question: 'What is the Cyrilic equivalent for sound "N"?',
-        answer: "Нн",
-        options: [
-            "Нн",
-            "Њњ",
-            "Вв",
-            "Ии"
-        ]
-    },
-    {
-        numb: 5,
-        question: 'What is the Cyrilic equivalent for sound "M"?',
-        answer: "",
-        options: [
-            "Шш",
-            "Пп",
-            "Жж",
-            "Мм"
-        ]
+    if (res.ok){
+        const data = await res.json();
+        return data;
+    } else{
+        throw new Error('Unable to retrieve questions')
     }
-]
+
+}
+
+
+function questionCounter(index, questions){
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} of ${questions.length} Questions`;
+}
+
+function headerScore(){
+    const headerScoreText = document.querySelector('.header-score');
+    headerScoreText.textContent = `Score: ${userScore} / ${questions.length}`;
+
+}
