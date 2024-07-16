5ddbba276f9159275fc36ffe53123b1bde122cae
branislav-bogosavac <branislav-bogosavac@byu.local>
2024-07-15 18:01:49 -0600

    quiz

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: 'What is the Cyrilic equivalent for letter "A"?',
+        answer: "Аа",
+        options: [
+            "Ее",
+            "Оо",
+            "Аа",
+            "Лл"
+        ]
+    },
+    {
+        numb: 2,
+        question: 'What is the Cyrilic equivalent for sound "R"?',
+        answer: "",
+        options: [
+            "Сс",
+            "Тт",
+            "Ђђ",
+            "Рр"
+        ]
+    },
+    {
+        numb: 3,
+        question: 'What is the Cyrilic equivalent for sound "Sh" as in "smaSHed"?',
+        answer: "Шш",
+        options: [
+            "Њњ",
+            "Кк",
+            "Шш",
+            "Чч"
+        ]
+    },
+    {
+        numb: 4,
+        question: 'What is the Cyrilic equivalent for sound "N"?',
+        answer: "Нн",
+        options: [
+            "Нн",
+            "Њњ",
+            "Вв",
+            "Ии"
+        ]
+    },
+    {
+        numb: 5,
+        question: 'What is the Cyrilic equivalent for sound "M"?',
+        answer: "",
+        options: [
+            "Шш",
+            "Пп",
+            "Жж",
+            "Мм"
+        ]
+    }
+]
--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+const btnSignup = document.querySelector('.btnSignup-popup');
+
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=>{
 
     wrapper.classList.add('active');
 })
 
+btnSignup.addEventListener('click',() => {
+    wrapper.classList.add('active-popup');
+    wrapper.classList.add('active');
+})
+
+
 loginLink.addEventListener('click', ()=>{
 
     wrapper.classList.remove('active');
+})
+
+btnPopup.addEventListener('click', ()=>{
+
+    wrapper.classList.add('active-popup');
+    wrapper.classList.remove('active');
+})
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
 })
