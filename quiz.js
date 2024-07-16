60895da6c7c63151c716fa182ee7fc094c909ba1
branislav-bogosavac <branislav-bogosavac@byu.local>
2024-07-15 18:30:50 -0600

    quiz

--- quiz.js
+++ quiz.js
@@ -24,4 +24,137 @@ continueBtn.onclick = () => {
     popupInfo.classList.remove('active');
     main.classList.remove('active');
     quizBox.classList.add('active');
+    showQuestions(0);
 }
+
+
+
+let questionCount = 0;
+
+const nextBtn = document.querySelector('.next-btn');
+const optionList = document.querySelector('.option-list');
+
+nextBtn.onclick = () => {
+    questionCount++;
+    if (questionCount < questions.length) {
+        showQuestions(questionCount);
+    } else {
+        console.log("End of questions");
+    }
+}
+
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = '';
+    for (let i = 0; i < questions[index].options.length; i++) {
+        optionTag += `<div class="option"><span>${questions[index].options[i]}</span></div>`;
+    }
+
+    optionList.innerHTML = optionTag;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let questions = [
+    {
+        numb: 1,
+        question: 'What is the Cyrilic equivalent for letter "A"?',
+        answer: "Аа",
+        options: [
+            "Ее",
+            "Оо",
+            "Аа",
+            "Лл"
+        ]
+    },
+    {
+        numb: 2,
+        question: 'What is the Cyrilic equivalent for sound "R"?',
+        answer: "",
+        options: [
+            "Сс",
+            "Тт",
+            "Ђђ",
+            "Рр"
+        ]
+    },
+    {
+        numb: 3,
+        question: 'What is the Cyrilic equivalent for sound "Sh" as in "smaSHed"?',
+        answer: "Шш",
+        options: [
+            "Њњ",
+            "Кк",
+            "Шш",
+            "Чч"
+        ]
+    },
+    {
+        numb: 4,
+        question: 'What is the Cyrilic equivalent for sound "N"?',
+        answer: "Нн",
+        options: [
+            "Нн",
+            "Њњ",
+            "Вв",
+            "Ии"
+        ]
+    },
+    {
+        numb: 5,
+        question: 'What is the Cyrilic equivalent for sound "M"?',
+        answer: "",
+        options: [
+            "Шш",
+            "Пп",
+            "Жж",
+            "Мм"
+        ]
+    }
+]
